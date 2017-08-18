445179df6deeaa1261415b2625736d3adef41ae9
zhuqianye <q835041058@gmail.com>
2017-08-18 16:36:53 +0800

    first commit, webpack config partly

---
+++ app/sub.js
@@ -0,0 +1,7 @@
+function generateText(){
+	var element=document.createElement('h2');
+	element.innerHTML="I'm son";
+	return element;
+}
+
+module.exports=generateText;
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+let path=require('path');
+let HtmlWebpackPlugin=require('html-webpack-plugin');
+
+let ROOT_PATH=path.resolve(__dirname);
+let APP_PATH=path.resolve(ROOT_PATH,'app');
+let BUILD_PATH=path.resolve(ROOT_PATH,'build');
+
+module.exports={
+	// 设置入口文件
+	entry:APP_PATH,
+	// 设置出口文件
+	output:{
+		path:BUILD_PATH,
+		filename:'bundle.js'
+	},
+	// 配置loader，使用rules而不是loaders
+	module:{
+
+	},
+	// 配置webpack插件，注意，它是个数组，其他人都是对象，与众不同
+	plugins:[
+		new HtmlWebpackPlugin({
+			title:'Hello world app'
+		})
+	]
+}
+
